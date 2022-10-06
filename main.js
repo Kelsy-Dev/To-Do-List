f9cf3f5de96554de0fd83f37851c955c90070de8
KLC <mkelsy@icloud.com>
2022-10-06 01:40:42 -0400

    Add eventlistener and classes

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
 const type = document.querySelector('input');
 const toDo = document.getElementById('create');
-
+const ul = document.querySelector('.list');
  
 toDo.addEventListener("keypress", (e) => {
     if(e.key === "Enter" && type.value.length > 0) {
 
-    const ul = document.querySelector('.list');
     const li = document.createElement('li');
 
     ul.appendChild(li);
     li.appendChild(document.createTextNode(type.value));
-    li.classList.add('addList');
+    li.classList.add('li');
     type.value = '';
     }
-    
 })
- 
+
+
+const li = document.querySelectorAll('li');
+
+for (let i = 0; i < li.length; i++) {
+
+    li[i].addEventListener("click", () => {
+        li[i].classList.toggle('done');
+        li[i].classList.toggle('addList')
+    })
+}
+
+//Implement .done through new tasks
+
+
+
+
+
