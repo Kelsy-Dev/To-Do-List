2510d114dfb56322e1e5ef66fa9988c5530880d4
KLC <mkelsy@icloud.com>
2022-10-05 22:36:57 -0400

    Add a textnode

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-
+const type = document.querySelector('input');
 const toDo = document.getElementById('create');
 
  
 toDo.addEventListener("keypress", (e) => {
-    if(e.key === "Enter") {
-        
-    const adding = document.createElement('li');
-    adding.appendChild(ul);
-    adding.classList.add('addList');
+    if(e.key === "Enter" && type.value.length > 0) {
+
+    const ul = document.querySelector('.list');
+    const li = document.createElement('li');
+
+    ul.appendChild(li);
+    li.appendChild(document.createTextNode(type.value));
+    li.classList.add('addList');
+    type.value = '';
     }
     
 })
-
-//Define ul or maybe add the classname? 
+ 
