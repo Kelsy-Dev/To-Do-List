8296b938fb24d6df45ccfcd82b15a2aeacb96479
KLC <mkelsy@icloud.com>
2022-10-07 01:25:41 -0400

    Add checklist function

--- main.js
+++ main.js
@@ -2,28 +2,46 @@ const type = document.querySelector('input');
 const toDo = document.getElementById('create');
 const ul = document.querySelector('.list');
  
-toDo.addEventListener("keypress", (e) => {
-    if(e.key === "Enter" && type.value.length > 0) {
+    toDo.addEventListener("keypress", (e) => {
+        if(e.key === "Enter" && type.value.length > 0) {
+    
+        const li = document.createElement('li');
+    
+        ul.appendChild(li);
+        li.appendChild(document.createTextNode(type.value));
+        li.classList.add('li');
+        type.value = '';
 
-    const li = document.createElement('li');
+        li.classList.add('cross');
+        li.addEventListener("click", () => {
+            li.classList.toggle('done');
+            li.classList.toggle('addList');
+        })
+        }  
+    })
 
-    ul.appendChild(li);
-    li.appendChild(document.createTextNode(type.value));
-    li.classList.add('li');
-    type.value = '';
+//Delete this after you are done
+const task = document.querySelectorAll('li');
+    
+    for (let i = 0; i < task.length; i++) {
+        
+        task[i].classList.add('cross');
+        task[i].addEventListener("click", () => {
+            task[i].classList.toggle('done');
+            task[i].classList.toggle('addList')
+        })
     }
+
+const crosscheck = document.querySelector('cross');
+
+crosscheck.addEventListener("click", () => {
+    console.log("working");
 })
 
 
-const li = document.querySelectorAll('li');
 
-for (let i = 0; i < li.length; i++) {
 
-    li[i].addEventListener("click", () => {
-        li[i].classList.toggle('done');
-        li[i].classList.toggle('addList')
-    })
-}
+
 
 //Implement .done through new tasks
 
