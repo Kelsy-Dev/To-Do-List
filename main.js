951f5b040934a408dbc9c8f9720589d83f23f607
KLC <mkelsy@icloud.com>
2022-10-13 00:21:44 -0400

    Adding dark mode

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const type = document.querySelector('#create');
 const toDo = document.getElementById('create');
 const tasklist = document.querySelector('.tasklist');
 const darkMode = document.querySelector('#icon');
+const moon = document.querySelector('.moon');
 
 const check = document.createElement('button');
 const cross = document.createElement('button');
@@ -20,36 +21,26 @@ const checkbttn = (btn) => {
     task.appendChild(btn);
     btn.appendChild(img);
 }
-
 const crossbttn = () => {
     cross.classList.add('cross');
-
     const imgC = document.createElement('img');
     imgC.src = "images/icon-cross.svg";
     cross.appendChild(imgC)
 }
+// *********************************
+// *********ADDING A NEW TASK*******
 
 toDo.addEventListener("keypress", (e) => {
     if(e.key === "Enter" && type.value.length > 0) {
-
-    task.classList.add('task');
     
     checkbttn(check);
     crossbttn();
-
-    const fuck = type.value;
-
-    items.type = "text";
-    items.name = "test";
-    items.setAttribute("test", "readonly");
   
     tasklist.appendChild(task);
     task.appendChild(items);
     task.appendChild(cross);
     items.classList.add('items');
-    items.innerText = fuck;
     task.classList.add('task');
-    // task.classList.add('cross');
     type.value = '';
 
     check.addEventListener("click", () => {
@@ -66,13 +57,34 @@ toDo.addEventListener("keypress", (e) => {
     } 
 })
 
-darkMode.addEventListener("click", () => {
-    items.classList.toggle('drkbtn');
-    cross.classList.toggle('drkbtn');
-    task.classList.toggle('drkbtn');
-    type.classList.toggle('drkbtn');
-    bdy.classList.toggle('drk');
-    check.classList.toggle('checkdrk');
+// **************************
+// *********DARK MODE********
 
-    console.log('works')
+moon.addEventListener("click", () => {
+    const sun = document.createElement('img');
+    if(moon) {
+        
+        items.classList.add('drkbtn');
+        cross.classList.add('drkbtn');
+        task.classList.add('drkbtn');
+        type.classList.add('drkbtn');
+        bdy.classList.add('drk');
+        check.classList.add('checkdrk');
+        darkMode.removeChild(moon);
+    
+        sun.src = "images/icon-sun.svg";
+        darkMode.appendChild(sun);
+        sun.classList.add('sun');
+        
+    } if(sun) { 
+        sun.addEventListener("click", () => {
+
+            console.log("turn light");
+        })
+    } return 
+    
 })
+
+// Add transitions on sun 
+//fix tasks
+//optimize/DRY your code
