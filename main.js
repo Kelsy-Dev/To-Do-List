7a6599215c7055cbc8b7fc89ae73756e289752fd
Kelsy <103698964+Kelsy-Dev@users.noreply.github.com>
2023-08-12 21:19:06 -0400

    DRY Code

--- main.js
+++ main.js
@@ -1,114 +1,53 @@
-const bdy = document.querySelector('body');
-const type = document.querySelector('#create');
-const toDo = document.getElementById('create');
-const tasklist = document.querySelector('.tasklist');
-const darkMode = document.querySelector('#icon');
-const moon = document.querySelector('.moon');
+const moon = document.querySelector(".moon");
+const sun = document.querySelector(".sun");
+const light = document.querySelector(".bg");
+const body = document.querySelector("body");
+const input = document.querySelector("input");
+const taskContainer = document.getElementById("taskContainer");
 
-// ***********************
-// *********BUTTONS*******
-
-// const checkbttn = (btn) => {
-//     btn.classList.add('check');
-//     const img = document.createElement('img');
-//     img.src = `images/icon-check.svg`
-//     task.appendChild(btn);
-//     btn.appendChild(img);
-// }
-// const crossbttn = () => {
-//     cross.classList.add('cross');
-//     const imgC = document.createElement('img');
-//     imgC.src = "images/icon-cross.svg";
-//     cross.appendChild(imgC)
-// }
-// *********************************
-// *********ADDING A NEW TASK*******
-
-toDo.addEventListener("keypress", (e) => {
-    if(e.key === "Enter" && type.value.length > 0) {
-
-        const task = document.createElement('div');
-        const items = document.createElement('input');
-
-    // checkbttn(check);
-    const check = document.createElement('button');
-    check.classList.add('check');
-    const img = document.createElement('img');
-    img.src = `images/icon-check.svg`
-    task.appendChild(check);
-    check.appendChild(img);
-
-    const cross = document.createElement('button');
-    cross.classList.add('cross');
-    const imgC = document.createElement('img');
-    imgC.src = "images/icon-cross.svg";
-    cross.appendChild(imgC)
-   
-    // creating new tasks
-    tasklist.appendChild(task);
-    task.appendChild(items);
-    task.appendChild(cross);
-    items.classList.add('items');
-    task.classList.add('task');
-    items.createTextNode('test');
-    type.value = '';
-
-    check.addEventListener("click", () => {
-        items.classList.toggle('done');
-        check.classList.toggle('ok');
-
-        console.log("Check button");
-    })
-    cross.addEventListener("click", () => {
-        tasklist.removeChild(task);
-        console.log("remove");
-    })
-
-    } 
-})
-
-// **************************
-// *********DARK MODE********
-
-// const vars = [items, cross, task, type]
-
-//     const switchMode = () => {
-//         for (let i=0; i<vars.length; i++) {
-//         vars[i].classList.add('drkbtn');
-        
-//         bdy.classList.add('drk');
-//         check.classList.add('checkdrk');
-//     }
-// }
-
-// moon.addEventListener("click", () => {
-//     const sun = document.createElement('img');
-//     if(moon) {
-        
-//         switchMode()
-//         darkMode.removeChild(moon);
+function switchMode() {
+        moon.classList.toggle("hide")
+        sun.classList.toggle('hide');
+        light.classList.toggle('bg');
+        body.classList.toggle('dark');
+        input.classList.toggle('darkLi');
+}
+switchMode();
     
-//         sun.src = "images/icon-sun.svg";
-//         darkMode.appendChild(sun);
-//         sun.classList.add('sun');
-//         console.log("turn dark");
+    inputBox.addEventListener("keypress", (e) => {
+        if(e.key === "Enter" && input.value.length > 0) {
+            console.log("adding task");
+
+            let newTask = document.createElement("li");
+            let span = document.createElement("span");
+
+            newTask.innerHTML = inputBox.value;
+            inputBox.value = '';
+
+            span.classList.add("cross");
+            newTask.classList.add("done");
+            
+            taskContainer.appendChild(newTask);
+            newTask.append(span);
+            
+        } saveData();
+    })
         
-//     } if(sun) { 
-//         sun.addEventListener("click", () => {
-//             items.classList.remove('drkbtn');
-//             cross.classList.remove('drkbtn');
-//             task.classList.remove('drkbtn');
-//             type.classList.remove('drkbtn');
-//             bdy.classList.remove('drk');
-//             check.classList.remove('checkdrk');
-//             darkMode.removeChild(sun);
-//             darkMode.appendChild(moon)
+taskContainer.addEventListener("click", (e) => {
+    if(e.target.tagName === 'LI' ) {
+        e.target.classList.toggle("check");
+        saveData();
+    } else if(e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}) 
 
-//             console.log("turn light");
-//         })
-//     } return 
-    
-// })
+function saveData() {
+    localStorage.setItem('data', taskContainer.innerHTML);
+}
 
-
-//optimize/DRY your code
+function showTask() {
+    taskContainer.innerHTML = localStorage.getItem('data');
+}
+showTask();
